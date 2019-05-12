202db347198b496fb0c488402973f0b3b38d6798
IsaSca <isasca@hotmail.co.uk>
2019-05-12 16:58:33 +0100

    Hella comments

--- src/Graph.java
+++ src/Graph.java
@@ -1,15 +1,37 @@
 import java.util.ArrayList;
 
+/**
+ * A collection of all vertices.
+ * @version 0.1
+ * @since 0.1
+ */
 public class Graph {
-    ArrayList<GraphNode> vertices = new ArrayList<>();
+    /**
+     * A list of all vertices in the graph.
+     */
+    ArrayList<Vertex> vertices = new ArrayList<>();
+
+    /**
+     * A method for determining whether a graph is Eulerian, and therefore efficient.
+     * @return True for eulerian, false otherwise.
+     */
     public boolean isEulerian() {
+        /**
+         * A counter for amount of odd vertices.
+         */
         int eulerNode = 0;
-        for (GraphNode g : vertices) {
-            if (g.getConnections().size() % 2 != 0) {
+        /**
+         * Checks each vertex in the vertices.
+         */
+        for (Vertex vertex : vertices) {
+            if (vertex.getConnections().size() % 2 != 0) {
                 eulerNode += 1;
             }
         }
-        if (eulerNode > 2) {
+        /**
+         * This will determine whether the graph is or isn't eulerian.
+         */
+        if (eulerNode == 2 || eulerNode == 0) {
             return true;
         }
         return false;
---
+++ src/Vertex.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+/**
+ * @author Isaac Scarisbrick
+ * @version 0.1
+ * @since 0.1
+ */
+public class Vertex {
+    private ArrayList<Vertex> connections = new ArrayList<>();
+    private int ID;
+    private int cost;
+
+    /**
+     * Adds a single vertex connection.
+     *@param g A Vertex to be added to connections.
+     *@since 0.1
+     */
+    public void addConnection(Vertex g) {
+        connections.add(g);
+    }
+
+    /**
+     * Adds a lot of vertices, hence the name.
+     * @since 0.1
+     * @param a The multiple vertices to be added to the connections.
+     */
+    public void addLots(ArrayList<Vertex> a) {
+        connections.addAll(a);
+    }
+
+    /**
+     * Finds a node based on given ID.
+     * @param ID The ID of the vertex you want to find.
+     * @return True for found, false for not.
+     */
+    public boolean findNode(int ID) {
+        if (this.ID == ID) {
+            return true;
+        } else {
+            for (Vertex g : connections) {
+                g.findNode(ID);
+            }
+        }
+        return false;
+    }
+
+    //BELOW HERE IS BORING SHIT
+
+    public Vertex(ArrayList<Vertex> connections, int ID, int cost) {
+        this.connections = connections;
+        this.ID = ID;
+        this.cost = cost;
+    }
+    public ArrayList<Vertex> getConnections() {
+        return connections;
+    }
+    public void setConnections(ArrayList<Vertex> connections) {
+        this.connections = connections;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
